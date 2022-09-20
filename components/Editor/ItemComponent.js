56ceb59db87500507aaccdc5a37d93c34892bcc3
KelvinQiu <kelvinqiu802@outlook.com>
2022-09-20 15:58:34 +0800

    fix: remove optional chain

--- components/Editor/ItemComponent.js
+++ components/Editor/ItemComponent.js
@@ -4,7 +4,7 @@ import styles from '../../styles/ItemComponent.module.css';
 
 function ItemComponent({ data }) {
   const Item = allComponents[data.tag];
-  if (typeof data.props?.children == 'object') {
+  if (typeof data.props.children == 'object') {
     return (
       <Item {...data.props}>
         <ItemComponent data={data.props.children} />
