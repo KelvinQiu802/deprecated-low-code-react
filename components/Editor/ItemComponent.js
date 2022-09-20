f9add0ab023e260a50c20b9e127ef56d1d646804
KelvinQiu <kelvinqiu802@outlook.com>
2022-09-20 15:34:49 +0800

    feat: item wrapper

--- components/Editor/ItemComponent.js
+++ components/Editor/ItemComponent.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import allComponents from '../../utils/allComponents';
+import styles from '../../styles/ItemComponent.module.css';
 
 function ItemComponent(props) {
   const Item = allComponents[props.tag];
   return (
-    <>
+    <div className={styles.wrapper}>
       <Item {...props.props} />
-    </>
+    </div>
   );
 }
 
