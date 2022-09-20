823f409ddb5aeed797cb5ab2c146267b46e9860c
KelvinQiu <kelvinqiu802@outlook.com>
2022-09-20 16:21:26 +0800

    feat: hover outline

--- components/Editor/ItemComponent.js
+++ components/Editor/ItemComponent.js
@@ -3,18 +3,40 @@ import allComponents from '../../utils/allComponents';
 import styles from '../../styles/ItemComponent.module.css';
 
 function ItemComponent({ data }) {
+  const [hover, setHover] = React.useState(false);
+
   const Item = allComponents[data.tag];
+  const cl = `${styles.wrapper} ${hover ? styles.hover : null}`;
+
+  const handleMouseEnter = (e) => {
+    e.stopPropagation();
+    setHover(true);
+  };
+  const handleMouseLeave = (e) => {
+    e.stopPropagation();
+    setHover(false);
+  };
+
   if (typeof data.props.children == 'object') {
     return (
-      <div className={styles.wrapper}>
+      <div
+        className={cl}
+        onMouseEnter={(e) => handleMouseEnter(e)}
+        onMouseLeave={(e) => handleMouseLeave(e)}
+      >
         <Item {...data.props}>
           <ItemComponent data={data.props.children} />
         </Item>
       </div>
     );
   }
+
   return (
-    <div className={styles.wrapper}>
+    <div
+      className={cl}
+      onMouseEnter={(e) => handleMouseEnter(e)}
+      onMouseLeave={(e) => handleMouseLeave(e)}
+    >
       <Item {...data.props} />
     </div>
   );
