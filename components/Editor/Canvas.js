cfcb03743199ed525afac071bd55a1cbec898f28
KelvinQiu <kelvinqiu802@outlook.com>
2022-09-20 15:04:34 +0800

    feat: set state

--- components/Editor/Canvas.js
+++ components/Editor/Canvas.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import styles from '../../styles/Canvas.module.css';
+import ItemComponent from './ItemComponent';
+import { nanoid } from 'nanoid';
 
-function Canvas() {
-  return <div className={styles.canvas}>Canvas</div>;
+function Canvas({ data }) {
+  return (
+    <div className={styles.canvas}>
+      {data.map((item) => (
+        <ItemComponent key={nanoid()} {...item} />
+      ))}
+    </div>
+  );
 }
 
 export default Canvas;
