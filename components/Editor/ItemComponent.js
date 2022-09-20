4c5d4c48dd3bd4c161913236d2879326584a34de
KelvinQiu <kelvinqiu802@outlook.com>
2022-09-20 15:51:44 +0800

    fix: render nested component

--- components/Editor/ItemComponent.js
+++ components/Editor/ItemComponent.js
@@ -3,10 +3,19 @@ import allComponents from '../../utils/allComponents';
 import styles from '../../styles/ItemComponent.module.css';
 
 function ItemComponent(props) {
+  console.log(props);
   const Item = allComponents[props.tag];
+  const data = props;
+  if (typeof data.props?.children == 'object') {
+    return (
+      <Item {...data.props}>
+        <ItemComponent {...data.props.children} />
+      </Item>
+    );
+  }
   return (
     <div className={styles.wrapper}>
-      <Item {...props.props} />
+      <Item {...data.props} />
     </div>
   );
 }
--- utils/allComponents.jsx
+++ utils/allComponents.jsx
@@ -2,18 +2,7 @@ import { Button, Paper } from '@mui/material';
 
 const allComponents = {
   Button: (props) => <Button {...props}>{props.text}</Button>,
-  Paper: (props) => {
-    let Item;
-    if (typeof props.children == 'object') {
-      Item = allComponents[props.children.tag];
-      return (
-        <Paper {...props}>
-          <Item {...props.children.props} />
-        </Paper>
-      );
-    }
-    return <Paper {...props}></Paper>;
-  },
+  Paper: (props) => <Paper {...props}></Paper>,
 };
 
 export default allComponents;
